720d874270be74e7054d6cff8974983bb4501605
Tony Chen <playggyygod@gmail.com>
2020-03-09 21:56:24 +0800

    Add View

--- ZUShop/urls.py
+++ ZUShop/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from shop_app.views import sayhello
+from shop_app.views import sayhello,hello3
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', sayhello),
+    path('hello3/', hello3),
 
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 
 # Create your views here.
 
 def sayhello(request):
     return HttpResponse("Hello Django!!")
+
+def hello3(request):
+    now = datetime.now()
+    return render(request, "hello3.html", locals())
 
 
